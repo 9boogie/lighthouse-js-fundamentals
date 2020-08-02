cbf9f9668fdf329cffa480a97d41a6902ce953f1
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-02 00:29:34 +0000

    Use conditionals to tell people about the weather

--- conditionals.js
+++ conditionals.js
@@ -1,5 +1,4 @@
-/*
-const raining = true;
+const raining = false;
 const cold = false;
 
 if (raining){
@@ -12,9 +11,9 @@ if (cold){
     console.log("Short sleeves are fine.");
 }
 
-console.log("Now You're ready to go outside!"); */
+console.log("Now You're ready to go outside!"); 
 
-const temperature = -3;
+const temperature = 50;
 
 if (temperature<0) {
     console.log("Make sure you pick out a scarf!");
@@ -24,4 +23,20 @@ if (temperature<0) {
     console.log("Short sleeve are fine.");
 }
 
-console.log("Now You're ready to go outside!");
+console.log("Now You're ready to go outside!"); 
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+    console.log("You are eligible to vote.");
+} 
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea...");
+}
+
+if(!raining){
+    console.log("Leave your umbrella at home!");
+}
